3efb147d4f9ef2e1830b36419a52a153ec1d8713
Camden Phalen <camden2112@gmail.com>
2016-04-06 23:15:45 -0400

    the calendar is now better than google calendar

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ var app = angular.module('happenstance', []);
 app.controller('MainController', [
   '$scope',
   function($scope){
-    $scope.test = 'Hello world!';
+    $scope.events = [
+      { name: 'event 1', startTime: 1, endTime: 4},
+      { name: 'event 2', startTime: 8, endTime: 12},
+      { name: 'event 3', startTime: 3, endTime: 6},
+      { name: 'event 4', startTime: 12, endTime: 1},
+      { name: 'event 5', startTime: 9, endTime: 10}
+    ];
   }
 ]);
