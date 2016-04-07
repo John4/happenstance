a0a8a1e1398dc02ed38d861282030d7be5134e14
Camden Phalen <camden2112@gmail.com>
2016-04-06 22:53:58 -0400

    destroy everything except readme

--- db/migrate/20160220033737_add_base_fields_to_event.rb
+++
@@ -1,7 +0,0 @@
-class AddBaseFieldsToEvent < ActiveRecord::Migration
-  def change
-    add_column :events, :title, :string
-    add_column :events, :start_time, :datetime
-    add_column :events, :end_time, :datetime
-  end
-end
